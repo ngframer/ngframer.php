79e9cc90a36ef33cb75ab26e0cb9961883081e1d
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-02-29 13:25:46 +0545

    Created sample database config file.
    Created sample database config file for database connection. Will be ignored on Git upload even if updated.

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ngframerphp\config;
+
+define("DB_NAME", "");
+define("DB_HOST", "");
+define("DB_PORT", 3306);
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_DSN", "mysql:host=".DB_HOST.";dbname=".DB_NAME.";");
