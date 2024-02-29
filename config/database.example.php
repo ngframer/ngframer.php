fe99b7a5f8429f6608436c5af2d83fcd7fe3a987
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-02-29 13:27:16 +0545

    Updated the database example config.
    Updated database example config file to resemble some model filler data.

--- config/database.example.php
+++ config/database.example.php
@@ -2,9 +2,9 @@
 
 namespace ngframerphp\config;
 
-define("DB_NAME", "");
+define("DB_NAME", "database_name");
 define("DB_HOST", "localhost");
 define("DB_PORT", 3306);
-define("DB_USER", "");
-define("DB_PASS", "");
+define("DB_USER", "database_user");
+define("DB_PASS", "database_pass_123");
 define("DB_DSN", "mysql:host=".DB_HOST.";dbname=".DB_NAME.";");
