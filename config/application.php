0b95711d35e52478e265859e873d13d047d22c89
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-04 18:40:57 +0545

    Added APPMODE and APPTYPE to application.

--- config/application.php
+++ config/application.php
@@ -3,5 +3,9 @@
 namespace ngframerphp\config\Application;
 
 define("ROOT", dirname(__DIR__));
+
 define("APPNAME", "NGFramerPHP");
 define("NAMESPACE", "app");
+
+define("APPMODE", ""); // Can be development or production.
+define("APPTYPE", ""); // Can be api or web.
