8638d7fa0a871b67413767f9a8c8febabde08a70
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-06 19:02:42 +0545

    Updated index page application config requirement.

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@
 error_reporting(E_ALL);
 
 // Load the autoloader and use namespace.
-require_once  __DIR__ . '/../config/application.php';
 require_once  __DIR__ . '/../vendor/autoload.php';
 
 // Start using the namespace.
