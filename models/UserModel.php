8837cfb1a484d3d773fba70acc0d197afde221df
neupkishor <114856857+neupkishor@users.noreply.github.com>
2024-06-26 20:50:31 +0545

    Created example database model.

---
+++ models/UserModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\models;
+
+use Exception;
+use NGFramer\NGFramerPHPBase\model\DbModel;
+
+class UserModel extends DbModel
+{
+    protected array $structure = [
+        'type' => 'table', 'name' => 'examples'
+    ];
+
+    protected array $fields = ['id', 'username', 'password'];
+    protected array $fillableFields = ['username', 'password'];
+    protected array $guardedFields = [];
+
+
+    /**
+     * Have as many custom functions as per the need.
+     * @throws Exception
+     */
+    public function create($username, $password): void
+    {
+        $this->insert(['username'=>$username, 'password'=>$password]);
+    }
+}
+
