f3a2003e37a534f0483f9a808be5e9e48e516295
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2024-11-27 14:17:18 +0545

    Renamed, updated examples of registry.

---
+++ Registry/ApiRegistry.php
@@ -0,0 +1,14 @@
+<?php
+
+use NGFramer\NGFramerPHPBase\Registry\RegistrySetter;
+
+// Creating a new instance of the RegistrySetter.
+$registry = new RegistrySetter();
+
+
+// Index pages.
+$registry->selectMethod('get')->selectPath('/')->setCallback([App\Controllers\Index::class, 'index']);
+$registry->selectMethod('get')->selectPath('/home')->setCallback([App\Controllers\Index::class, 'index']);
+
+// Error pages.
+$registry->selectMethod('any')->selectPath('any')->setCallback([Error::class, 'index']);
