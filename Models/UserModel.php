bab6b3b56826c0a2520abebd1074bd641cead517
Kishor Neupane <114856857+neupkishor@users.noreply.github.com>
2025-01-16 21:57:10 +0545

    Updated method of using Models.

--- Models/UserModel.php
+++ Models/UserModel.php
@@ -3,26 +3,21 @@
 namespace App\Models;
 
 use Exception;
-use NGFramer\NGFramerPHPBase\model\DbModel;
+use App\Schemas\DbTableName;
+use NGFramer\NGFramerPHPBase\Model\DbModel;
 
 class UserModel extends DbModel
 {
-    protected array $structure = [
-        'type' => 'table', 'name' => 'examples'
-    ];
-
-    protected array $fields = ['id', 'username', 'password'];
-    protected array $fillableFields = ['username', 'password'];
-    protected array $guardedFields = [];
-
-
     /**
      * Have as many custom functions as per the need.
      * @throws Exception
      */
-    public function create($username, $password): void
+    public function create($username, $password): array
     {
-        $this->insert(['username'=>$username, 'password'=>$password]);
+        // Use the schema for accessing the tables.
+        DbTableName::init()->insert([]);
+        // Respond as per the need.
+        return [];
     }
 }
 
